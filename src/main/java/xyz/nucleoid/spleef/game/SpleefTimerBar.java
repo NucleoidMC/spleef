aa624dda61598c4f328c07d7d3b5c02875353446
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-06-13 03:53:37 -0400

    Improve the title style of the timer bar (#17)

--- src/main/java/xyz/nucleoid/spleef/game/SpleefTimerBar.java
+++ src/main/java/xyz/nucleoid/spleef/game/SpleefTimerBar.java
@@ -3,6 +3,8 @@ package xyz.nucleoid.spleef.game;
 import net.minecraft.entity.boss.BossBar;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
 import xyz.nucleoid.plasmid.widget.BossBarWidget;
 import xyz.nucleoid.plasmid.widget.GlobalWidgets;
 
@@ -14,24 +16,29 @@ public final class SpleefTimerBar {
     }
 
     static SpleefTimerBar create(GlobalWidgets widgets) {
-        LiteralText title = new LiteralText("Dropping in...");
+        Text title = getBarTitle(new LiteralText("Dropping in..."));
         return new SpleefTimerBar(widgets.addBossBar(title, BossBar.Color.GREEN, BossBar.Style.NOTCHED_10));
     }
 
     public void update(long ticksUntilDrop, long totalTicksUntilDrop) {
         if (ticksUntilDrop % 20 == 0) {
-            this.widget.setTitle(this.getText(ticksUntilDrop));
+            this.widget.setTitle(this.getDroppingText(ticksUntilDrop));
             this.widget.setProgress((float) ticksUntilDrop / totalTicksUntilDrop);
         }
     }
 
-    private Text getText(long ticksUntilDrop) {
+    private Text getDroppingText(long ticksUntilDrop) {
         long secondsUntilDrop = ticksUntilDrop / 20;
 
         long minutes = secondsUntilDrop / 60;
         long seconds = secondsUntilDrop % 60;
         String time = String.format("%02d:%02d", minutes, seconds);
 
-        return new LiteralText("Dropping in: " + time + "...");
+        return getBarTitle(new LiteralText("Dropping in: " + time + "..."));
+    }
+
+    private static Text getBarTitle(Text customText) {
+        Text gameName = new TranslatableText("game.spleef.spleef").formatted(Formatting.BOLD);
+        return new LiteralText("").append(gameName).append(" - ").append(customText).formatted(Formatting.GREEN);
     }
 }
