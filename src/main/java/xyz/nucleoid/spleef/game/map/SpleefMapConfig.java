4dcd163d7f31898e41365423762621ea7a4bb717
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-12-28 04:01:11 -0500

    Update minigame to Minecraft 1.18.1

--- src/main/java/xyz/nucleoid/spleef/game/map/SpleefMapConfig.java
+++ src/main/java/xyz/nucleoid/spleef/game/map/SpleefMapConfig.java
@@ -4,7 +4,6 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.block.Blocks;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
-import net.minecraft.world.gen.stateprovider.SimpleBlockStateProvider;
 import xyz.nucleoid.spleef.game.map.shape.renderer.MapShapeRenderer;
 
 public record SpleefMapConfig(
@@ -19,10 +18,10 @@ public record SpleefMapConfig(
         return instance.group(
                 Codec.INT.fieldOf("levels").forGetter(config -> config.levels),
                 Codec.INT.fieldOf("level_height").forGetter(map -> map.levelHeight),
-                BlockStateProvider.TYPE_CODEC.optionalFieldOf("wall_provider", new SimpleBlockStateProvider(Blocks.STONE_BRICKS.getDefaultState())).forGetter(SpleefMapConfig::wallProvider),
-                BlockStateProvider.TYPE_CODEC.optionalFieldOf("floor_provider", new SimpleBlockStateProvider(Blocks.SNOW_BLOCK.getDefaultState())).forGetter(SpleefMapConfig::floorProvider),
-                BlockStateProvider.TYPE_CODEC.optionalFieldOf("ceiling_provider", new SimpleBlockStateProvider(Blocks.BARRIER.getDefaultState())).forGetter(SpleefMapConfig::ceilingProvider),
-                BlockStateProvider.TYPE_CODEC.optionalFieldOf("lava_provider", new SimpleBlockStateProvider(Blocks.LAVA.getDefaultState())).forGetter(SpleefMapConfig::lavaProvider),
+                BlockStateProvider.TYPE_CODEC.optionalFieldOf("wall_provider", BlockStateProvider.of(Blocks.STONE_BRICKS)).forGetter(SpleefMapConfig::wallProvider),
+                BlockStateProvider.TYPE_CODEC.optionalFieldOf("floor_provider", BlockStateProvider.of(Blocks.SNOW_BLOCK)).forGetter(SpleefMapConfig::floorProvider),
+                BlockStateProvider.TYPE_CODEC.optionalFieldOf("ceiling_provider", BlockStateProvider.of(Blocks.BARRIER)).forGetter(SpleefMapConfig::ceilingProvider),
+                BlockStateProvider.TYPE_CODEC.optionalFieldOf("lava_provider", BlockStateProvider.of(Blocks.LAVA)).forGetter(SpleefMapConfig::lavaProvider),
                 MapShapeRenderer.REGISTRY_CODEC.fieldOf("shape").forGetter(config -> config.shape)
         ).apply(instance, SpleefMapConfig::new);
     });
