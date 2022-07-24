456d1e34347eb84cd6c7833f23f88a1238ec5624
Gegy <gegy.dev@gmail.com>
2022-07-24 14:13:43 +0200

    Refactor: extract SpleefShape builder for building from known size

--- src/main/java/xyz/nucleoid/spleef/game/map/shape/SpleefShape.java
+++ src/main/java/xyz/nucleoid/spleef/game/map/shape/SpleefShape.java
@@ -3,23 +3,15 @@ package xyz.nucleoid.spleef.game.map.shape;
 import xyz.nucleoid.spleef.game.map.SpleefLevel;
 
 public final class SpleefShape {
-    static final byte EMPTY = 0;
-    static final byte FILL = 1;
-    static final byte OUTLINE = 2;
+    private static final byte EMPTY = 0;
+    private static final byte FILL = 1;
+    private static final byte OUTLINE = 2;
 
-    public final int minX, minZ;
-    public final int maxX, maxZ;
-    private final int width;
-
+    public final Bounds bounds;
     private final byte[] shape;
 
-    SpleefShape(int minX, int minZ, int maxX, int maxZ, byte[] shape) {
-        this.minX = minX;
-        this.minZ = minZ;
-        this.maxX = maxX;
-        this.maxZ = maxZ;
-
-        this.width = this.maxX - this.minX + 1;
+    private SpleefShape(Bounds bounds, byte[] shape) {
+        this.bounds = bounds;
         this.shape = shape;
     }
 
@@ -33,9 +25,9 @@ public final class SpleefShape {
 
     private void forEachOf(ForEach handler, byte of) {
         var shape = this.shape;
-        for (int z = this.minZ; z <= this.maxZ; z++) {
-            for (int x = this.minX; x <= this.maxX; x++) {
-                int index = this.index(x, z);
+        for (int z = this.bounds.minZ(); z <= this.bounds.maxZ(); z++) {
+            for (int x = this.bounds.minX(); x <= this.bounds.maxX(); x++) {
+                int index = this.bounds.index(x, z);
                 if (shape[index] == of) {
                     handler.accept(x, z);
                 }
@@ -56,15 +48,8 @@ public final class SpleefShape {
     }
 
     private byte get(int x, int z) {
-        int index = this.index(x, z);
+        int index = this.bounds.index(x, z);
         return index != -1 ? this.shape[index] : EMPTY;
-    }
-
-    private int index(int x, int z) {
-        if (x < this.minX || z < this.minZ || x > this.maxX || z > this.maxZ) {
-            return -1;
-        }
-        return (x - this.minX) + (z - this.minZ) * this.width;
     }
 
     public SpleefLevel toLevel(int y) {
@@ -73,5 +58,56 @@ public final class SpleefShape {
 
     public interface ForEach {
         void accept(int x, int z);
+    }
+
+    public static class Builder {
+        private final Bounds bounds;
+        private final byte[] shape;
+
+        public Builder(int minX, int minZ, int maxX, int maxZ) {
+            this.bounds = new Bounds(minX, minZ, maxX, maxZ);
+            this.shape = new byte[this.bounds.count()];
+        }
+
+        public void putFill(int x, int z) {
+            this.shape[this.index(x, z)] = FILL;
+        }
+
+        public void putOutline(int x, int z) {
+            this.shape[this.index(x, z)] = OUTLINE;
+        }
+
+        private int index(int x, int z) {
+            int index = this.bounds.index(x, z);
+            if (index == -1) {
+                throw new IllegalArgumentException("Point (" + x + "; " + z + ") out of bounds " + this.bounds);
+            }
+            return index;
+        }
+
+        public SpleefShape build() {
+            return new SpleefShape(this.bounds, this.shape);
+        }
+    }
+
+    private record Bounds(int minX, int minZ, int maxX, int maxZ) {
+        public int index(int x, int z) {
+            if (x < this.minX || z < this.minZ || x > this.maxX || z > this.maxZ) {
+                return -1;
+            }
+            return (x - this.minX) + (z - this.minZ) * this.width();
+        }
+
+        public int width() {
+            return this.maxX - this.minX + 1;
+        }
+
+        public int height() {
+            return this.maxZ - this.minZ + 1;
+        }
+
+        public int count() {
+            return this.width() * this.height();
+        }
     }
 }
